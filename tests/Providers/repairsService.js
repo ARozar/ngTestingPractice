2381c732ccfa45f8c87d10fa9abd9e0b5b3a6a83
Andrew de Rozario <andrew_de_rozario@andrews-mbp.home>
2015-06-14 14:26:38 +0100

    adding basic (and flawed) test to the repairs service

--- tests/Providers/repairsService.js
+++ tests/Providers/repairsService.js
@@ -1,21 +1,29 @@
 describe('repairs service should',function(){
 
-	var mockResource
+	var mockResource;
+	
+	var querySpy = jasmine.createSpy('mockRepairResource');
+	mockResource  = function(){
+		return{
+			query:querySpy
+		};
+	};
+
 
 	beforeEach(module('app',function($provide){
 		
-		$provide.value(); 
+		$provide.value('$resource',mockResource); 
 	}));
 
 	 it('get all from resource get all function',inject(function(repairsService){
 	 	//arrange
-
+		
 
 	 	//act
-
+		repairsService.getAll(function(){});
 
 	 	//assert
-
+		expect(querySpy).toHaveBeenCalled();
 
 	 }));
 });
