dfbdb7628e01721f8ee44ab70e7b55b8d0c14c60
Andrew de Rozario <andrew_de_rozario@andrews-mbp.home>
2015-06-14 13:56:18 +0100

    adding basic use if resource

--- app/Providers/repairsService.js
+++ app/Providers/repairsService.js
@@ -1,7 +1,15 @@
 angular.module('app')
 .factory('repairsService',['$resource',function($resource){
+		
+		var repairsResource = $resource('/Repairs/:id', null,
+		    {
+		        'update': { method:'PUT' }
+		    });
 
-		var getAll = function(){}; 
+
+		var getAll = function(callback){
+			repairsResource.query(callback);
+		}; 
 
 		return {
 			getAll:getAll
--- tests/Providers/repairsService.js
+++ tests/Providers/repairsService.js
@@ -1,7 +1,11 @@
 describe('repairs service should',function(){
 
+	var mockResource
 
-	beforeEach(module('app'));
+	beforeEach(module('app',function($provide){
+		
+		$provide.value(); 
+	}));
 
 	 it('get all from resource get all function',inject(function(repairsService){
 	 	//arrange
